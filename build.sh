1a17679ffd78d77de6d18278b63680d630e22020
risto <risto.kowaczewski@gmail.com>
2021-09-15 14:32:42 +0200

    building

--- build.sh
+++ build.sh
@@ -1,6 +1,3 @@
-docker build . -t tracardi/tracardi-gui:0.6.0
-docker push tracardi/tracardi-gui:0.6.0
-
-#docker build . -t tracardi/tracardi-gui
-#docker push tracardi/tracardi-gui
+docker build . --no-cache  -t tracardi/tracardi-gui
+docker push tracardi/tracardi-gui
 
