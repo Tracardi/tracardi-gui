56358dc75eb8bd678822131358e3dfa5bf2fa68b
risto <risto.kowaczewski@gmail.com>
2021-09-17 00:47:38 +0200

    Profile heat map

--- src/components/elements/details/ProfileDetails.js
+++ src/components/elements/details/ProfileDetails.js
@@ -11,6 +11,9 @@ import DataBrowsingList from "../../pages/DataBrowsingList";
 import LineChartElement from "../charts/LineChart";
 import PropTypes from "prop-types";
 import PiiDetails from "./PiiDetails";
+import HeatMap from "@uiw/react-heat-map";
+// import HeatMap from "@uiw/react-heat-map";
+// import Tooltip from '@uiw/react-tooltip';
 
 export default function ProfileDetails({data}) {
 
@@ -36,20 +39,20 @@ export default function ProfileDetails({data}) {
 
     const query = encodeParams();
 
-    const onLoadEventDataRequest = (filterQuery) => {
-        return {
-            url: '/event/select/range',
-            method: "post",
-            data: filterQuery
-        }
-    }
-    const onLoadEventHistogramRequest = (filterQuery) => {
-        return {
-            url: '/event/select/histogram',
-            method: "post",
-            data: filterQuery
-        }
-    }
+    // const onLoadEventDataRequest = (filterQuery) => {
+    //     return {
+    //         url: '/event/select/range',
+    //         method: "post",
+    //         data: filterQuery
+    //     }
+    // }
+    // const onLoadEventHistogramRequest = (filterQuery) => {
+    //     return {
+    //         url: '/event/select/histogram',
+    //         method: "post",
+    //         data: filterQuery
+    //     }
+    // }
 
     const onLoadSessionHistogramRequest = (filterQuery) => {
         return {
@@ -86,17 +89,52 @@ export default function ProfileDetails({data}) {
                     </div>
                 </TabCase>
                 <TabCase id={2}>
-                    <DataBrowsingList
-                        label="List of Profiles"
-                        onLoadDataRequest={onLoadEventDataRequest}
-                        timeFieldLabel="timestamp"
-                        filterFields={['metadata.time.utc']}
-                        timeField={(row) => [row.metadata.time.utc]}
-                        initQuery={query}
-                    >
-                        <LineChartElement onLoadRequest={onLoadEventHistogramRequest(query)}
-                                          columns={[{label: "events", color: "#039be5", stackId: "events"}]}/>
-                    </DataBrowsingList>
+                    <div style={{padding: 20}}>
+                        <HeatMap value={[
+                            { date: '2016/01/11', count:2 },
+                            ...[...Array(17)].map((_, idx) => ({ date: `2016/01/${idx + 10}`, count: idx })),
+                            ...[...Array(17)].map((_, idx) => ({ date: `2016/02/${idx + 10}`, count: idx })),
+                            { date: '2016/04/12', count:2 },
+                            { date: '2016/05/01', count:5 },
+                            { date: '2016/05/02', count:5 },
+                            { date: '2016/05/03', count:1 },
+                            { date: '2016/05/04', count:11 },
+                            { date: '2016/05/08', count:32 },
+                            { date: '2016/11/08', count:32 },
+                        ]}
+                                 startDate={new Date('2016/01/01')}
+                                 width={1150}
+                                 height={180}
+                                 rectSize={18}
+                                 rectProps={{
+                                     rx: 10
+                                 }}
+                                 panelColors={{
+                                     0: '#b3e5fc',
+                                     2: '#4fc3f7',
+                                     4: '#03a9f4',
+                                     10: '#0288d1',
+                                     20: '#1976d2',
+                                     30: '#0d47a1',
+                                     50: '#002171',
+                                     100: '#000'
+                                 }}
+                                 space = {3}
+                                 legendRender={()=>{}}
+
+                        />
+                    </div>
+                    {/*<DataBrowsingList*/}
+                    {/*    label="List of Profiles"*/}
+                    {/*    onLoadDataRequest={onLoadEventDataRequest}*/}
+                    {/*    timeFieldLabel="timestamp"*/}
+                    {/*    filterFields={['metadata.time.utc']}*/}
+                    {/*    timeField={(row) => [row.metadata.time.utc]}*/}
+                    {/*    initQuery={query}*/}
+                    {/*>*/}
+                    {/*    <LineChartElement onLoadRequest={onLoadEventHistogramRequest(query)}*/}
+                    {/*                      columns={[{label: "events", color: "#039be5", stackId: "events"}]}/>*/}
+                    {/*</DataBrowsingList>*/}
                 </TabCase>
                 <TabCase id={3}>
                     <DataBrowsingList
