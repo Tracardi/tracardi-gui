296bb8a39d525a12373065ece59924bcb85e436d
Daniel Johnson <danjohnson77@gmail.com>
2021-09-21 20:42:55 -0700

    integrated ObjectList component into Instances.js

--- src/components/pages/Instances.js
+++ src/components/pages/Instances.js
@@ -1,3 +1,50 @@
+import { useEffect, useState } from "react";
+import { request } from "../../remote_api/uql_api_endpoint";
+import ObjectList from "../elements/lists/ObjectList";
+
 export default function Instances() {
-    return "Not implemented"
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [errors, setErrors] = useState(null);
+
+  useEffect(() => {
+    request(
+      {
+        url: "/instances",
+        method: "GET",
+      },
+      (l) => {
+        setLoading(l);
+      }, // sets l=true when loading, l=false when ready
+      (e) => {
+        setErrors(e);
+      }, // runs on error
+      (response) => {
+        // on response ready
+        if (response) {
+          console.log(response.data);
+          setData(response.data);
+        }
+      }
+    );
+  }, [loading]);
+
+  if (data) {
+    return (
+      <ObjectList
+        data={data}
+        label="INSTANCES"
+        errors={errors}
+        loading={loading}
+        timeField={(row) => [row.timestamp]}
+        timeFieldLabel="Timestamp"
+      />
+    );
+  }
+
+  if (errors) {
+    return "ERROR";
+  }
+
+  return null;
 }
