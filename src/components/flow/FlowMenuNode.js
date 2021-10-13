efce18fe93e15eda9f96453c555654b4aa9afd5a
risto <risto.kowaczewski@gmail.com>
2021-10-13 10:45:04 +0200

    Plugin id added for remote validation.

--- src/components/flow/FlowMenuNode.js
+++ src/components/flow/FlowMenuNode.js
@@ -1,9 +1,9 @@
 import React from "react";
 import FlowNodeIcons from "./FlowNodeIcons";
 
-export default function FlowMenuNode({onDragStart, data}) {
+export default function FlowMenuNode({onDragStart, row}) {
     return <div className="menuNode" onDragStart={(event) =>
-        onDragStart(event,data)} draggable>
-        <FlowNodeIcons icon={data?.metadata?.icon}/> <span style={{marginLeft: 5}}>{data?.metadata?.name}</span>
+        onDragStart(event, row)} draggable>
+        <FlowNodeIcons icon={row.plugin?.metadata?.icon}/> <span style={{marginLeft: 5}}>{row.plugin?.metadata?.name}</span>
     </div>
 }
