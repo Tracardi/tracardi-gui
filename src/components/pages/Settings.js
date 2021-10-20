3e73e54dd85d1b18dc69912c4210c979198087bb
risto <risto.kowaczewski@gmail.com>
2021-10-21 00:31:44 +0200

    TimeInput refactored.

--- src/components/pages/Settings.js
+++ src/components/pages/Settings.js
@@ -2,9 +2,11 @@ import React from "react";
 import ListOfDottedInputs from "../elements/forms/ListOfDottedInputs";
 import CopyTraitsForm from "../elements/forms/CopyTraitsForm";
 import DottedPathInput from "../elements/forms/inputs/DottedPathInput";
+import TimeInput from "../elements/forms/inputs/TimeInput";
 
 export default function Settings() {
     return <>
+        <TimeInput />
         <DottedPathInput value={"ala.kk"} onChange={(v)=>console.log(v)} forceMode={2} width={300}/>
         <ListOfDottedInputs onChange={(x)=>console.log(x)}/>
         <CopyTraitsForm onChange={(x)=>console.log(x)}/>
