6cd6dabb5f397d6523734bd6156830b78b8d8df3
risto <risto.kowaczewski@gmail.com>
2021-11-05 23:21:24 +0100

    Pii Error

--- src/components/elements/details/PiiDetails.js
+++ src/components/elements/details/PiiDetails.js
@@ -18,7 +18,7 @@ const PiiDetails = ({data}) => {
                 </div>
                 <div>
                     <header className="Name">
-                        {data.traits?.private?.pii?.name} {data.traits?.private?.pii?.surname ? data.traits?.private?.pii?.surname : "Anonymous"}
+                        {data.pii?.name} {data.pii?.surname ? data.pii?.surname : "Anonymous"}
                     </header>
                     <div>
                         Last event: {data.metadata?.time?.lastVisit ? data.metadata.time?.lastVisit : "not available"}
@@ -31,24 +31,24 @@ const PiiDetails = ({data}) => {
             <div className="PiiData">
                 <section>
                     <div>
-                        <FaBirthdayCake style={{marginRight: 5}}/> {data.traits?.private?.pii?.birthDate ? data.traits?.private?.pii?.birthDate : "not available"}
+                        <FaBirthdayCake style={{marginRight: 5}}/> {data.pii?.birthDate ? data.pii?.birthDate : "not available"}
                     </div>
                     <div>
-                        <FiMail style={{marginRight: 5}}/> {data.traits?.private?.pii?.email ? data.traits?.private?.pii?.email : "not available"}
+                        <FiMail style={{marginRight: 5}}/> {data.pii?.email ? data.pii?.email : "not available"}
                     </div>
                     <div>
-                        <BiPhoneCall style={{marginRight: 5}}/>{data.traits?.private?.pii?.telephone ? data.traits?.private?.pii?.telephone : 'not available'}
+                        <BiPhoneCall style={{marginRight: 5}}/>{data.pii?.telephone ? data.pii?.telephone : 'not available'}
                     </div>
                 </section>
                 <aside>
                     <div>
-                        <FaFacebookSquare size={20} style={{marginRight: 8}}/> {data.traits?.private?.pii?.facebook ? data.traits?.private?.pii?.facebook : "not available"}
+                        <FaFacebookSquare size={20} style={{marginRight: 8}}/> {data.pii?.facebook ? data.pii?.facebook : "not available"}
                     </div>
                     <div>
-                        <VscTwitter size={20} style={{marginRight: 8}}/> {data.traits?.private?.pii?.twitter ? data.traits?.private?.pii?.twitter: "not available"}
+                        <VscTwitter size={20} style={{marginRight: 8}}/> {data.pii?.twitter ? data.pii?.twitter: "not available"}
                     </div>
                     <div>
-                        <FaWhatsapp style={{marginRight: 5}}/>{data.traits?.private?.pii?.whatsapp ? data.traits?.private?.pii?.whatsapp : "not available"}
+                        <FaWhatsapp style={{marginRight: 5}}/>{data.pii?.whatsapp ? data.pii?.whatsapp : "not available"}
                     </div>
                 </aside>
             </div>
