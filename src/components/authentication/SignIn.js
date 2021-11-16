25b907c1f893b35203521de0c353e51f0b2cbdcc
PiotrKukuc12 <PiotrKukuc12@gmail.com>
2021-11-16 14:08:21 +0100

    remove imports

--- src/components/authentication/SignIn.js
+++ src/components/authentication/SignIn.js
@@ -3,8 +3,6 @@ import {Redirect, useLocation} from "react-router-dom";
 import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
 import Link from '@material-ui/core/Link';
 import Grid from '@material-ui/core/Grid';
 import Box from '@material-ui/core/Box';
