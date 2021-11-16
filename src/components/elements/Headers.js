981abb49734df1882355c43dee1967265b5b3659
PiotrKukuc12 <PiotrKukuc12@gmail.com>
2021-11-16 13:08:31 +0100

    Add headers for event list

--- src/components/elements/Headers.js
+++ src/components/elements/Headers.js
@@ -4,3 +4,7 @@ import "./Headers.css";
 export function MiniHeader({...props}) {
     return <div className="MiniHeader">{props.children}</div>
 }
+
+export const SemiHeader = ({children}) => {
+    return <div className="SemiHeader">{children}</div>
+}
