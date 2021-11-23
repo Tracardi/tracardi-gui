248f425b50dbf89fabf9da306ad96e54c467ff18
risto <risto.kowaczewski@gmail.com>
2021-11-23 22:57:40 +0100

    Cleanup

--- src/components/pages/TryOut.js
+++ src/components/pages/TryOut.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ListOfDottedInputs from "../elements/forms/ListOfDottedInputs";
-import CopyTraitsForm from "../elements/forms/CopyTraitsForm";
 import DottedPathInput from "../elements/forms/inputs/DottedPathInput";
 import TimeInput from "../elements/forms/inputs/TimeInput";
 import KeyValueForm from "../elements/forms/KeyValueForm";
@@ -13,6 +12,5 @@ export default function TryOut() {
         <TimeInput />
         <DottedPathInput value={"ala.kk"} onChange={(v)=>console.log(v)} forceMode={2} width={300}/>
         <ListOfDottedInputs onChange={(x)=>console.log(x)}/>
-        <CopyTraitsForm onChange={(x)=>console.log(x)}/>
         </>
 }
