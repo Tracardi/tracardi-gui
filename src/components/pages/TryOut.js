5ac5ee90a7070d687016bc3769a92555fdc32bd2
Daniel Johnson <danjohnson77@gmail.com>
2021-12-01 11:39:55 -0800

    Reset Tryout.js to previous state

--- src/components/pages/TryOut.js
+++ src/components/pages/TryOut.js
@@ -1,22 +1,18 @@
 import React from "react";
-// import ListOfDottedInputs from "../elements/forms/ListOfDottedInputs";
-// import DottedPathInput from "../elements/forms/inputs/DottedPathInput";
-// import TimeInput from "../elements/forms/inputs/TimeInput";
-// import KeyValueForm from "../elements/forms/KeyValueForm";
-// import IconSelector from "../elements/IconSelector";
-import ConsentTypeForm from "../elements/forms/ConsentTypeForm";
-import ConsentTypeList from "../elements/lists/ConsentTypeList";
+import ListOfDottedInputs from "../elements/forms/ListOfDottedInputs";
+import DottedPathInput from "../elements/forms/inputs/DottedPathInput";
+import TimeInput from "../elements/forms/inputs/TimeInput";
+import KeyValueForm from "../elements/forms/KeyValueForm";
+import IconSelector from "../elements/IconSelector";
 
 export default function TryOut() {
   return (
     <div>
-      {/* <IconSelector value="alert" onChange={(ic) => console.log(ic)}/>
-        <KeyValueForm value={{kw:"value"}} onChange={(v)=>console.log(v)}/>
-        <TimeInput />
-        <DottedPathInput value={"ala.kk"} onChange={(v)=>console.log(v)} forceMode={2} width={300}/>
-        <ListOfDottedInputs onChange={(x)=>console.log(x)}/> */}
-      <ConsentTypeList />
-      {/* <ConsentTypeForm /> */}
+      <IconSelector value="alert" onChange={(ic) => console.log(ic)} />
+      <KeyValueForm value={{ kw: "value" }} onChange={(v) => console.log(v)} />
+      <TimeInput />
+      <DottedPathInput value={"ala.kk"} onChange={(v) => console.log(v)} forceMode={2} width={300} />
+      <ListOfDottedInputs onChange={(x) => console.log(x)} />
     </div>
   );
 }
