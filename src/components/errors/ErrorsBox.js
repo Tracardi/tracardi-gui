0347399bf6fc42412e433b02dd8ba84ce18df98f
risto <risto.kowaczewski@gmail.com>
2022-01-14 00:29:53 +0100

    ErrorsBox fix

--- src/components/errors/ErrorsBox.js
+++ src/components/errors/ErrorsBox.js
@@ -7,21 +7,25 @@ export default function ErrorsBox({errorList, fillWidth, style}) {
     const visual = (typeof fillWidth === "undefined") ? "ErrorsBox NotFullErrorBox" : "ErrorsBox";
 
     const Message = ({location, msg, type}) => {
-        if(location) {
+        if (location) {
             return <>{location}: {msg} [{type}]</>
         }
         return <>{msg} [{type}]</>
     }
 
-    const displayErrors = (errorLst) => errorLst.map(({msg, loc, type}, index)=>{
-        const location = Array.isArray(loc) ? loc.join(".") : null
-        return <li key={index} title={location}>
-            <Message location={location} msg={msg} type={type}/>
-        </li>
-    })
+    const displayErrors = (errorLst) => {
+        if(Array.isArray(errorLst)) {
+            return errorLst.map(({msg, loc, type}, index) => {
+                const location = Array.isArray(loc) ? loc.join(".") : null
+                return <li key={index} title={location}>
+                    <Message location={location} msg={msg} type={type}/>
+                </li>
+            })
+        }
+    }
 
     const displayBox = (errorLst) => {
-        if(errorLst) {
+        if (errorLst) {
             return <div className={visual} style={style}>
                 <div style={{width: 40}}><VscError size={40}/></div>
                 <div className="ErrorDetails">
