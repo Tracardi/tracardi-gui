34aa8c9b2acf20282b1a613c626b31e5d4bf6f35
Bruce Hopkins <behopkinsjr@gmail.com>
2022-02-01 15:59:27 +0100

    Generic table component based on react-window (#90)
    * started table compontent
    
    * Added table component
    
    * feat: added table component
    
    * feat: added table component
    
    * fix: added prop types
    
    * refactor: changed from list to grid
    
    * fix: deleted code in index
    
    * refactor: added grids
    
    Co-authored-by: Bruce-Hopkins <behopkins@gmail.com>

---
+++ src/components/elements/table/table.js
@@ -0,0 +1,57 @@
+import React, { Fragment } from "react";
+import { FixedSizeGrid as Grid } from 'react-window';
+import propTypes from 'prop-types';
+
+import "./table.css"
+
+/* 
+  Usage example:
+  <Table rows={[1, 2, 3]} columns={[1, 2, 2]}/> 
+*/ 
+class Table extends React.Component {
+  render() {
+    const height = this.props.height | 200,
+    width = this.props.width | 300,
+    rows = this.props.rows,
+    rowHeight = this.props.rowHeight | 20,
+    columns = this.props.columns,
+    columnWidth = this.props.columnWidth | 100;
+  
+    const Cell = ({ columnIndex, rowIndex, style }) => {
+      let label;
+        label = `${rows[rowIndex]}, ${columns[columnIndex]}`;
+      return (
+        <div className="list-item" style={style}>
+          {label}
+        </div>
+      );
+    }
+  
+    return (
+      <Fragment>
+        <Grid
+          height={height}
+          width={width}
+          columnCount={columns.length}
+          columnWidth={columnWidth}
+          rowCount={rows.length}
+          rowHeight={rowHeight}
+        >
+          {Cell}
+        </Grid>
+      </Fragment>
+    );
+  
+  }
+}
+
+Table.propTypes = {
+  height: propTypes.number,
+  width: propTypes.number,
+  rows: propTypes.array.isRequired,
+  colums: propTypes.array.isRequired,
+  columnWidth: propTypes.number,
+  rowHeight: propTypes.number
+}
+
+export default Table;
