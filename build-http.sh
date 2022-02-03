d3eae7e305d39399ca2b7a894f02a516e2da4f01
risto <risto.kowaczewski@gmail.com>
2022-02-03 10:50:50 +0100

    Version 0.6.1 Release

--- build-http.sh
+++ build-http.sh
@@ -1,3 +1,4 @@
-docker build . --no-cache -t tracardi/tracardi-gui
-docker push tracardi/tracardi-gui
+docker build . --no-cache -t tracardi/tracardi-gui:0.6.1
+docker push tracardi/tracardi-gui:0.6.1
+
 
