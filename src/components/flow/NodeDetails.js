78a1d470b6a2b962d3ccc641b7f62d36770caf5e
risto <risto.kowaczewski@gmail.com>
2022-02-07 20:45:55 +0100

    New right tab node details.

--- src/components/flow/NodeDetails.js
+++ src/components/flow/NodeDetails.js
@@ -51,91 +51,94 @@ export function NodeDetails({node, onConfig, onRuntimeConfig, onLabelSet}) {
 
     return (
         <div className="NodeDetails">
-            <div className="Title">
-                <FilterTextField label="Node name"
-                                 initValue={node?.data?.metadata?.name}
-                                 onSubmit={onLabelSet}
-                                 onChange={(event) => onLabelSet(event.target.value)}/>
-                <span>
+            <div className="NodeDetailsIcons">
                     <IconButton label="Info" onClick={() => setTab(0)} selected={tab === 0} size="large">
                             <BsInfoCircle size={22}/>
                     </IconButton>
-                    {node?.data?.metadata?.pro === true && <IconButton
-                        label="Tracardi Pro Editor"
-                        onClick={() => setTab(5)}
-                        selected={tab === 5}
-                        size="large">
-                        <BsStar size={22}/>
-                    </IconButton>}
-                    {node?.data?.spec?.form && <IconButton
-                        label="Config Editor"
-                        onClick={() => setTab(3)}
-                        selected={tab === 3}
-                        size="large">
-                        <GoSettings size={22}/>
-                    </IconButton>}
-                    {node?.data?.spec?.init && <IconButton
-                        label="Json Config"
-                        onClick={() => setTab(2)}
-                        selected={tab === 2}
-                        size="large">
-                        <VscTools size={22}/>
-                    </IconButton>}
-                    {node?.data?.metadata && <IconButton
-                        label="Advanced Runtime Editor"
-                        onClick={() => setTab(6)}
-                        selected={tab === 6}
-                        size="large">
-                        <VscRunErrors size={22}/>
-                    </IconButton>}
-                    <IconButton label="Raw" onClick={() => setTab(4)} selected={tab === 4} size="large">
+                {node?.data?.metadata?.pro === true && <IconButton
+                    label="Tracardi Pro Editor"
+                    onClick={() => setTab(5)}
+                    selected={tab === 5}
+                    size="large">
+                    <BsStar size={22}/>
+                </IconButton>}
+                {node?.data?.spec?.form && <IconButton
+                    label="Config Editor"
+                    onClick={() => setTab(3)}
+                    selected={tab === 3}
+                    size="large">
+                    <GoSettings size={22}/>
+                </IconButton>}
+                {node?.data?.spec?.init && <IconButton
+                    label="Json Config"
+                    onClick={() => setTab(2)}
+                    selected={tab === 2}
+                    size="large">
+                    <VscTools size={22}/>
+                </IconButton>}
+                {node?.data?.metadata && <IconButton
+                    label="Advanced Runtime Editor"
+                    onClick={() => setTab(6)}
+                    selected={tab === 6}
+                    size="large">
+                    <VscRunErrors size={22}/>
+                </IconButton>}
+                <IconButton label="Raw" onClick={() => setTab(4)} selected={tab === 4} size="large">
                             <VscJson size={22}/>
                     </IconButton>
-                    </span>
             </div>
-            <div className="Pane">
-                {tab === 0 && <NodeInfo node={node} onLabelSet={onLabelSet}/>}
-                {tab === 2 && node?.data?.spec?.init &&
-                <NodeInitJsonForm
-                    pluginId={node?.data?.spec?.id}
-                    formSchema={node?.data?.spec?.form}
-                    init={node?.data?.spec?.init}
-                    manual={node?.data?.spec?.manual}
-                    onSubmit={handleInitSubmit}
-                />}
-                {tab === 3 && node?.data?.spec?.form &&
-                <NodeInitForm
-                    pluginId={node?.data?.spec?.id}
-                    init={node?.data?.spec?.init}
-                    formSchema={node?.data?.spec?.form}
-                    onSubmit={handleInitSubmit}
-                />}
+            <div className="NodeDetailsContent">
+                <div className="Title">
+                    <FilterTextField label="Node name"
+                                     initValue={node?.data?.metadata?.name}
+                                     onSubmit={onLabelSet}
+                                     onChange={(event) => onLabelSet(event.target.value)}/>
 
-                {tab === 4 && <ConsoleView label="Action raw data" data={node}/>}
+                </div>
+                <div className="Pane">
+                    {tab === 0 && <NodeInfo node={node} onLabelSet={onLabelSet}/>}
+                    {tab === 2 && node?.data?.spec?.init &&
+                    <NodeInitJsonForm
+                        pluginId={node?.data?.spec?.id}
+                        formSchema={node?.data?.spec?.form}
+                        init={node?.data?.spec?.init}
+                        manual={node?.data?.spec?.manual}
+                        onSubmit={handleInitSubmit}
+                    />}
+                    {tab === 3 && node?.data?.spec?.form &&
+                    <NodeInitForm
+                        pluginId={node?.data?.spec?.id}
+                        init={node?.data?.spec?.init}
+                        formSchema={node?.data?.spec?.form}
+                        onSubmit={handleInitSubmit}
+                    />}
 
-                {node?.data?.metadata?.pro === true && tab === 5 && <TracardiProPluginForm
-                    init={node?.data?.spec?.init}
-                    onSubmit={handleInitSubmit}
-                />
-                }
+                    {tab === 4 && <ConsoleView label="Action raw data" data={node}/>}
 
-                {tab === 6 && node?.data?.spec && <NodeRuntimeConfigForm
-                    pluginId={node?.data?.spec?.id}
-                    value={
-                        {
-                            skip: node?.data?.spec?.skip || false,
-                            block_flow: node?.data?.spec?.block_flow || false,
-                            on_connection_error_repeat: node?.data?.spec?.on_connection_error_repeat || "0",
-                            run_in_background: node?.data?.spec?.run_in_background || false,
-                            on_error_continue: node?.data?.spec?.on_error_continue || false,
-                            join_input_payload: node?.data?.spec?.join_input_payload || false,
-                            append_input_payload: node?.data?.spec?.append_input_payload || false,
-                            run_once: node?.data?.spec?.run_once || {value: "", ttl: 0, type: "value", enabled: false},
-                        }
+                    {node?.data?.metadata?.pro === true && tab === 5 && <TracardiProPluginForm
+                        init={node?.data?.spec?.init}
+                        onSubmit={handleInitSubmit}
+                    />
                     }
-                    onChange={onRuntimeConfig}
-                />}
 
+                    {tab === 6 && node?.data?.spec && <NodeRuntimeConfigForm
+                        pluginId={node?.data?.spec?.id}
+                        value={
+                            {
+                                skip: node?.data?.spec?.skip || false,
+                                block_flow: node?.data?.spec?.block_flow || false,
+                                on_connection_error_repeat: node?.data?.spec?.on_connection_error_repeat || "0",
+                                run_in_background: node?.data?.spec?.run_in_background || false,
+                                on_error_continue: node?.data?.spec?.on_error_continue || false,
+                                join_input_payload: node?.data?.spec?.join_input_payload || false,
+                                append_input_payload: node?.data?.spec?.append_input_payload || false,
+                                run_once: node?.data?.spec?.run_once || {value: "", ttl: 0, type: "value", enabled: false},
+                            }
+                        }
+                        onChange={onRuntimeConfig}
+                    />}
+
+                </div>
             </div>
         </div>
     );
