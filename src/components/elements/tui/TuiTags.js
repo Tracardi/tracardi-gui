d7e5c764ca224096c67da6ceece9e05cf43b6de5
DawidekZagajnik <60825727+DawidekZagajnik@users.noreply.github.com>
2022-02-24 11:33:18 +0100

    event tagging site (#99)

--- src/components/elements/tui/TuiTags.js
+++ src/components/elements/tui/TuiTags.js
@@ -2,5 +2,5 @@ import Chip from "@mui/material/Chip";
 import React from "react";
 
 export default function TuiTags({tags}) {
-    return Array.isArray(tags) && tags.map((tag, index) => <Chip label={tag} key={index} style={{marginLeft: 5}}/>)
+    return Array.isArray(tags) && tags.map((tag, index) => <Chip label={tag} key={index} style={{marginLeft: 5, marginTop: 10}}/>)
 }
