ebaf1f4f9f20fc43c1e8ff3a639f328983385996
risto <risto.kowaczewski@gmail.com>
2022-05-10 23:16:11 +0200

    Imports

--- src/components/pages/ImportTasks.js
+++ src/components/pages/ImportTasks.js
@@ -1,3 +1,6 @@
+import NotImplemented from "../elements/misc/NotImplemented";
+import React from "react";
+
 export default function ImportTasks() {
-    return "Not implemented"
+    return <NotImplemented>Not implemented</NotImplemented>
 }
