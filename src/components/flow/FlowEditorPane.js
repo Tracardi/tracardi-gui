74a5856df7379f080525b9fff866cf360c51df70
Dawid Kruk <krukdawid27@gmail.com>
2022-06-29 23:44:33 +0200

    flow centering bug fix (#163)

--- src/components/flow/FlowEditorPane.js
+++ src/components/flow/FlowEditorPane.js
@@ -561,6 +561,7 @@ export function FlowEditorPane(
                             zoomOnDoubleClick={false}
                             zoomOnScroll={false}
                             panOnScroll={true}
+                            defaultPosition={[700, 100]} // set position so point (0, 0) is always visible
                             onElementsRemove={onElementsRemove}
                             onElementClick={onElementClick}
                             onNodeDoubleClick={onNodeDoubleClick}
