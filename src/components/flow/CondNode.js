104e4b355ff15c86f7d6dd9314f99cb6c049b2a1
risto <risto.kowaczewski@gmail.com>
2022-07-12 22:57:04 +0200

    Fix for https://github.com/Tracardi/tracardi/issues/544

--- src/components/flow/CondNode.js
+++ src/components/flow/CondNode.js
@@ -3,7 +3,7 @@ import {Handle} from 'react-flow-renderer';
 import './FlowNode.css';
 import ThresholdIcon from "./ThresholdIcon";
 import {isObject} from "../../misc/typeChecking";
-import {ExecutionSeqNumber} from "./NodeAlerts";
+import {ErrorNumber, ExecutionSeqNumber, WarningNumber} from "./NodeAlerts";
 import FlowNodeIcons from "./FlowNodeIcons";
 
 
@@ -15,7 +15,7 @@ const CondNodeDynamic = ({data}) => {
         const [showDesc, setShowDesc] = useState(false);
 
         return <div className="NodePortContainer">
-            {showHint && <span className="InputPortHint PortHint">{value}</span> }
+            {showHint && <span className="InputPortHint PortHint">{value}</span>}
             {showDesc && doc && <span className="InputPortDesc PortHint">{doc}</span>}
 
             <Handle
@@ -24,8 +24,13 @@ const CondNodeDynamic = ({data}) => {
                 position="top"
                 id={value}
                 onMouseOver={() => setShowHint(true)}
-                onMouseOut={() => {setShowHint(false); setShowDesc(false);}}
-                onClick={()=>{setShowDesc(true)}}
+                onMouseOut={() => {
+                    setShowHint(false);
+                    setShowDesc(false);
+                }}
+                onClick={() => {
+                    setShowDesc(true)
+                }}
             />
 
         </div>
@@ -58,8 +63,13 @@ const CondNodeDynamic = ({data}) => {
                     position="bottom"
                     id={value}
                     onMouseOver={() => setShowHint(true)}
-                    onMouseOut={() => {setShowHint(false); setShowDesc(false);}}
-                    onClick={()=>{setShowDesc(true)}}
+                    onMouseOut={() => {
+                        setShowHint(false);
+                        setShowDesc(false);
+                    }}
+                    onClick={() => {
+                        setShowDesc(true)
+                    }}
                 />
                 {showHint && <span className="OutputPortHint PortHint">{value}</span>}
                 {showDesc && doc && <span className="OutputPortDesc PortHint">{doc}</span>}
@@ -84,29 +94,42 @@ const CondNodeDynamic = ({data}) => {
     }
 
     const nodeClass = (data?.metadata?.selected === true) ? "CondNode DebugNode" : "CondNode"
-    const nodeStyle = (data?.spec?.skip===true || data?.spec?.block_flow===true) ? {borderColor: "#ccc", color: "#999"} : {}
-    const portStyle = (data?.spec?.skip===true || data?.spec?.block_flow===true) ? {borderColor: "#ccc"} : {borderColor: "#1565c0", borderWidth: 2}
+    const nodeStyle = (data?.spec?.skip === true || data?.spec?.block_flow === true) ? {
+        borderColor: "#ccc",
+        color: "#999"
+    } : {}
+    const portStyle = (data?.spec?.skip === true || data?.spec?.block_flow === true) ? {borderColor: "#ccc"} : {
+        borderColor: "#1565c0",
+        borderWidth: 2
+    }
 
     return (
         <div className="CondContainer" style={{gap: 15}}>
             <div style={{display: "flex", alignItems: "center"}}>
                 {Array.isArray(data?.spec?.init?.event_types) && <div className="NodeInboundEvents">
-                    {data?.spec?.init?.event_types.map((eventObj, idx)  => <span className="Event" key={idx}>{eventObj.name}</span>)}
+                    {data?.spec?.init?.event_types.map((eventObj, idx) => <span className="Event"
+                                                                                key={idx}>{eventObj.name}</span>)}
                 </div>}
                 <div style={{position: "relative"}}>
                     {data?.spec?.run_once?.enabled && <ThresholdIcon/>}
+                    {data?.debugging?.node?.warnings > 0 && data?.debugging?.node?.errors === 0 &&
+                    <WarningNumber data={data}/>}
+                    {data?.debugging?.node?.errors > 0 && <ErrorNumber data={data}/>}
                     <Inputs spec={data?.spec} documentation={data?.metadata?.documentation?.inputs} style={portStyle}/>
                     <ExecutionSeqNumber data={data} style={{top: 21, right: -14, zIndex: 2}}/>
                     <div className={nodeClass} style={nodeStyle}>
                         <div className="NodePadding">
-                            <span style={{transform: "rotate(45deg)"}}><FlowNodeIcons icon={data?.metadata?.icon} size={24}/></span>
+                            <span style={{transform: "rotate(45deg)"}}><FlowNodeIcons icon={data?.metadata?.icon}
+                                                                                      size={24}/></span>
                         </div>
                     </div>
-                    <Outputs spec={data?.spec} documentation={data?.metadata?.documentation?.outputs} style={portStyle}/>
+                    <Outputs spec={data?.spec} documentation={data?.metadata?.documentation?.outputs}
+                             style={portStyle}/>
 
                 </div>
             </div>
-            <div className="CondTitle">{data?.metadata?.name}</div>
+            {data?.debugging?.node?.errors === 0 && data?.debugging?.node?.warnings === 0 &&
+            <div className="CondTitle">{data?.metadata?.name}</div>}
         </div>
     );
 };
