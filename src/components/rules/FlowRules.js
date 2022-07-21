2f7cc2a5a274d57158de41aaf97fd51022cc5627
DawidekZagajnik <krukdawid27@gmail.com>
2022-07-21 21:15:27 +0200

    rules deleting button

--- src/components/rules/FlowRules.js
+++ src/components/rules/FlowRules.js
@@ -2,11 +2,13 @@ import React, {useEffect} from "react";
 import RuleRow from "../elements/lists/rows/RuleRow";
 import {asyncRemote} from "../../remote_api/entrypoint";
 import CircularProgress from "@mui/material/CircularProgress";
+import { useConfirm } from "material-ui-confirm";
 
 export default function FlowRules({flowName, id, refresh}) {
 
     const [rules, setRules] = React.useState([]);
     const [loading, setLoading] = React.useState(false);
+    const confirm = useConfirm();
 
     useEffect(() => {
             setLoading(true);
@@ -32,9 +34,27 @@ export default function FlowRules({flowName, id, refresh}) {
         [id, refresh]
     )
 
+    const onDelete = (id, name) => {
+        confirm({
+            title: `Are you sure you want to delete rule ${name}?`,
+            description: "This action cannot be undone."
+        })
+        .then(() => {
+            asyncRemote({
+                method: "DELETE",
+                url: `/rule/${id}`
+            })
+            .then(_ => {
+                setRules(rules.filter(rule => rule.id !== id));
+            })
+            .catch(_ => {})
+        })
+        .catch(() => {})
+    }
+
     const RulesList = ({flow, rules}) => {
         return rules.map((rule, index) => {
-            return <RuleRow data={rule} flow={flow} key={index}/>
+            return <RuleRow data={rule} flow={flow} key={index} onDelete={onDelete}/>
         })
     }
 
