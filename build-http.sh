9c68a0fba6e90956a616330fa8443ef3f73f5e4b
risto <risto.kowaczewski@gmail.com>
2022-08-01 13:22:08 +0200

    Version 0.7.1

--- build-http.sh
+++ build-http.sh
@@ -1,4 +1,4 @@
 git rev-parse HEAD > public/revision.txt
-docker build . --no-cache  -t tracardi/tracardi-gui
-docker push tracardi/tracardi-gui
+docker build . --no-cache  -t tracardi/tracardi-gui:0.7.1
+docker push tracardi/tracardi-gui:0.7.1
 
--- build-https.sh
+++ build-https.sh
@@ -1,3 +1,3 @@
 git rev-parse HEAD > public/revision.txt
-docker build . -f Dockerfile-https --no-cache -t tracardi/tracardi-gui-https
+docker build . -f Dockerfile-https --no-cache -t tracardi/tracardi-gui-https:0.7.1
 
