b3a6abe46f856b96d96761789b54b1133ac654ad
Dawid Kruk <krukdawid27@gmail.com>
2022-08-03 12:36:29 +0200

    resource documentation (#206)

--- src/components/flow/actions/MdManual.js
+++ src/components/flow/actions/MdManual.js
@@ -1,16 +1,16 @@
 import React, {useEffect, useState} from "react";
 import MarkdownElement from "../../elements/misc/MarkdownElement";
 import "./MdManual.css";
-import {getApiUrl} from "../../../remote_api/entrypoint";
+import { asyncRemote } from "../../../remote_api/entrypoint";
 
-const MdManual = ({mdFile}) => {
+const MdManual = ({mdFile, basePath='/manual/en/docs/flow/actions/'}) => {
 
     const [page,setPage] = useState('');
 
     async function loadMdFile(fileName) {
         try {
-            const response = await fetch(getApiUrl()+'/manual/en/docs/flow/actions/'+fileName+'.md?'+ Math.random());
-            return await response.text();
+            const response = await asyncRemote({url: `${basePath}${fileName}.md?${Math.random()}`})
+            return await response.data;
         } catch (e) {
             return e.toString()
         }
