de40fb35c0318bc95fc48200aac2a5fe668e08d4
risto <risto.kowaczewski@gmail.com>
2022-09-01 00:33:32 +0200

    Tracardi Pro

--- src/components/elements/lists/TracardiProAvailableServicesList.js
+++ src/components/elements/lists/TracardiProAvailableServicesList.js
@@ -7,33 +7,33 @@ import ServiceCard from "../cards/ServiceCard";
 import CenteredCircularProgress from "../progress/CenteredCircularProgress";
 import ErrorsBox from "../../errors/ErrorsBox";
 
-const TracardiProAvailableServicesList = ({query="", category="", onServiceClick}) => {
+const TracardiProAvailableServicesList = ({services = null, onServiceClick}) => {
 
-    const [services, setServices] = useState(null);
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState(null);
+    // const [services, setServices] = useState(null);
+    // const [loading, setLoading] = useState(false);
+    // const [error, setError] = useState(null);
 
-    useEffect(() => {
-        setLoading(true);
-        asyncRemote({
-            url: `/tpro/available_services?query=${query}&category=${category}`,
-            method: "GET",
-        }).then((response) => {
-            setServices(response.data)
-        }).catch((e) => {
-            setError(getError(e))
-        }).finally(() => {
-            setLoading(false);
-        })
-    }, [query, category])
-
-    if (error) {
-        return <ErrorsBox errorList={error} />
-    }
-
-    if(loading) {
-        return <CenteredCircularProgress label="Connecting Tracardi PRO"/>
-    }
+    // useEffect(() => {
+    //     setLoading(true);
+    //     asyncRemote({
+    //         url: `/tpro/available_services?query=${query}&category=${category}`,
+    //         method: "GET",
+    //     }).then((response) => {
+    //         setServices(response.data)
+    //     }).catch((e) => {
+    //         setError(getError(e))
+    //     }).finally(() => {
+    //         setLoading(false);
+    //     })
+    // }, [query, category])
+    //
+    // if (error) {
+    //     return <ErrorsBox errorList={error} />
+    // }
+    //
+    // if(loading) {
+    //     return <CenteredCircularProgress label="Connecting Tracardi PRO"/>
+    // }
 
     return <div className="TracardiProAvailableServicesList">
 
