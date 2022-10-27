a7d57bc89b1dfe90df044dd55108dc6d61e8b8eb
risto <risto.kowaczewski@gmail.com>
2022-10-28 01:00:34 +0200

    Reference highlight in JSO editor

--- src/components/elements/editors/JsonEditor.js
+++ src/components/elements/editors/JsonEditor.js
@@ -2,17 +2,54 @@ import "ace-builds/src-noconflict/ace";
 import "ace-builds/src-noconflict/mode-json";
 import "ace-builds/src-noconflict/mode-text";
 import "ace-builds/src-noconflict/theme-tomorrow";
-import React, {Suspense} from "react";
+import "ace-builds/src-noconflict/ext-language_tools"
+import "./JsonEditor.css";
+
+import React, {Suspense, useEffect, useRef} from "react";
 import CenteredCircularProgress from "../progress/CenteredCircularProgress";
+import ReferencedJsonMode from "./highlight/references";
 
 const AceEditor = React.lazy(() => import('react-ace'))
 
-export default function JsonEditor({onChange, value, height}) {
+export default function JsonEditor({onChange, value, height, autocomplete=true}) {
+
+    const aceEditor = useRef();
+
+    useEffect(() => {
+        if(aceEditor.current) {
+            aceEditor.current.editor.getSession().setMode(new ReferencedJsonMode())
+        }
+    },[aceEditor.current])
+
+    let options = {
+        useWorker: false
+    }
+
+    if(autocomplete) {
+        options = {
+            useWorker: false,
+            enableBasicAutocompletion: [{
+                getCompletions: (editor, session, pos, prefix, callback) => {
+                    callback(null, [
+                        {value: 'payload@', score: 1, meta: 'Payload data reference'},
+                        {value: 'event@', score: 2, meta: 'Event data reference'},
+                        {value: 'session@', score: 3, meta: 'Session data reference'},
+                        {value: 'profile@', score: 2, meta: 'Profile data reference'},
+                        {value: 'flow@', score: 2, meta: 'Flow data reference'},
+                        {value: 'memory@', score: 2, meta: 'Memory data reference'},
+                    ]);
+                },
+            }],
+            enableSnippets: false,
+            enableLiveAutocompletion: true
+        }
+    }
 
     return <Suspense fallback={<CenteredCircularProgress/>}>
         <AceEditor
             mode="json"
             theme="tomorrow"
+            ref={aceEditor}
             fontSize={16}
             // onLoad={(d)=>console.log(d)}
             onChange={onChange}
@@ -21,9 +58,12 @@ export default function JsonEditor({onChange, value, height}) {
             editorProps={{$blockScrolling: true}}
             width="100%"
             height={height ? height : "260px"}
-            setOptions={{
-                useWorker: false
-            }}
+            setOptions={options}
+            commands={[{   // commands is array of key bindings.
+                name: 'Help', //name for the key binding.
+                bindKey: {win: 'Ctrl-h', mac: 'Command-h'}, //key combination used for the command.
+                exec: () => { console.log('key-binding used')}  //function to execute when keys are pressed.
+            }]}
         />
     </Suspense>
 }
---
+++ src/components/elements/editors/highlight/references.js
@@ -0,0 +1,85 @@
+import "ace-builds/src-noconflict/mode-java";
+
+export class CustomHighlightRules extends window.ace.acequire(
+    "ace/mode/text_highlight_rules"
+).TextHighlightRules {
+    constructor() {
+        super();
+        this.$rules = {
+                start: [
+                    {
+                        token: "reference",
+                        regex: '["](profile|memory|session|payload|event|flow)[@].*["]'
+                    },
+                    {
+                        token: "variable",
+                        regex: '["](?:(?:\\\\.)|(?:[^"\\\\]))*?["]\\s*(?=:)'
+                    }, {
+                        token: "string",
+                        regex: '"',
+                        next: "string"
+                    }, {
+                        token: "constant.numeric",
+                        regex: "0[xX][0-9a-fA-F]+\\b"
+                    }, {
+                        token: "constant.numeric",
+                        regex: "[+-]?\\d+(?:(?:\\.\\d*)?(?:[eE][+-]?\\d+)?)?\\b"
+                    }, {
+                        token: "constant.language.boolean",
+                        regex: "(?:true|false)\\b"
+                    }, {
+                        token: "text",
+                        regex: "['](?:(?:\\\\.)|(?:[^'\\\\]))*?[']"
+                    }, {
+                        token: "comment",
+                        regex: "\\/\\/.*$"
+                    }, {
+                        token: "comment.start",
+                        regex: "\\/\\*",
+                        next: "comment"
+                    }, {
+                        token: "paren.lparen",
+                        regex: "[[({]"
+                    }, {
+                        token: "paren.rparen",
+                        regex: "[\\])}]"
+                    }, {
+                        token: "punctuation.operator",
+                        regex: /[,]/
+                    }, {
+                        token: "text",
+                        regex: "\\s+"
+                    }
+                ],
+                "string": [
+                    {
+                        token: "constant.language.escape",
+                        regex: /\\(?:x[0-9a-fA-F]{2}|u[0-9a-fA-F]{4}|["\\\/bfnrt])/
+                    }, {
+                        token: "string",
+                        regex: '"|$',
+                        next: "start"
+                    }, {
+                        defaultToken: "string"
+                    }
+                ],
+                "comment": [
+                    {
+                        token: "comment.end",
+                        regex: "\\*\\/",
+                        next: "start"
+                    }, {
+                        defaultToken: "comment"
+                    }
+                ]
+            };
+    }
+}
+
+export default class ReferencedJsonMode extends window.ace.acequire("ace/mode/json")
+    .Mode {
+    constructor() {
+        super();
+        this.HighlightRules = CustomHighlightRules;
+    }
+}
