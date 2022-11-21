2df891fd92b686d7382985e7cb457303c48a9d04
risto <risto.kowaczewski@gmail.com>
2022-11-21 12:43:08 +0100

    New design - Fixes consent page.

--- src/components/pages/Consents.js
+++ src/components/pages/Consents.js
@@ -1,10 +1,13 @@
-import React, {useCallback} from "react";
+import React, {useCallback, useState} from "react";
 import "../elements/lists/CardBrowser.css";
 import CardBrowser from "../elements/lists/CardBrowser";
 import SquareCard from "../elements/lists/cards/SquareCard";
 import {VscLaw} from "react-icons/vsc";
 import ConsentDetails from "../elements/details/ConsentDetails";
 import ConsentForm from "../elements/forms/ConsentForm";
+import BrowserRow from "../elements/lists/rows/BrowserRow";
+import {asyncRemote} from "../../remote_api/entrypoint";
+import {useConfirm} from "material-ui-confirm";
 
 
 export default function  Consents() {
@@ -12,8 +15,26 @@ export default function  Consents() {
     const urlFunc= useCallback((query) => ('/consents/type/by_tag' + ((query) ? "?query=" + query : "")),[]);
     const addFunc = useCallback((close) => <ConsentForm onSaveComplete={close}/>,[]);
     const detailsFunc= useCallback((id, close) => <ConsentDetails id={id} onDeleteComplete={close} onEditComplete={close}/>, [])
+    const [refresh, setRefresh] = useState(0);
+    const confirm = useConfirm();
 
-    const flows = (data, onClick) => {
+    const handleDelete = async (id) => {
+        confirm({title: "Do you want to delete this consent?", description: "This action can not be undone."})
+            .then(async () => {
+                    try {
+                        await asyncRemote({
+                            url: '/consent/type/' + id,
+                            method: "delete"
+                        })
+                        setRefresh(refresh+1)
+                    } catch (e) {
+                        console.error(e)
+                    }
+                }
+            )
+    }
+
+    const cards = (data, onClick) => {
         return data?.grouped && Object.entries(data?.grouped).map(([category, plugs], index) => {
             return <div className="CardGroup" key={index}>
                 <header>{category}</header>
@@ -24,7 +45,6 @@ export default function  Consents() {
                                            icon={<VscLaw size={45}/>}
                                            status={row?.enabled}
                                            name={row?.name}
-                                           description={row?.description}
                                            onClick={() => onClick(row?.id)}
                         />
                     })}
@@ -33,11 +53,31 @@ export default function  Consents() {
         })
     }
 
+    const rows = (data, onClick) => {
+        return data?.grouped && Object.entries(data?.grouped).map(([category, plugs], index) => {
+            return <div className="RowGroup" style={{width: "100%"}} key={index}>
+                <header>{category}</header>
+                <div>
+                    {plugs.map((row, subIndex) => {
+                        return <BrowserRow key={index + "-" + subIndex}
+                                           id={row?.id}
+                                           data={{...row, icon: "consent"}}
+                                           onClick={onClick}
+                                           onDelete={handleDelete}
+                        />
+                    })}
+                </div>
+            </div>
+        })
+    }
+
     return <CardBrowser
+        defaultLayout="row"
         label="Consent types"
         description="List of defined consent types. You may filter this list by consent name in the upper search box."
         urlFunc={urlFunc}
-        cardFunc={flows}
+        cardFunc={cards}
+        rowFunc={rows}
         buttomLabel="New consent type"
         buttonIcon={<VscLaw size={20}/>}
         drawerDetailsWidth={900}
