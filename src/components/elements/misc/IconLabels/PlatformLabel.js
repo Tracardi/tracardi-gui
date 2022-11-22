8b01d9c2a0dee4a7c31e5846583979392d3bebcf
risto <risto.kowaczewski@gmail.com>
2022-11-22 22:21:14 +0100

    New design refactoring

--- src/components/elements/misc/IconLabels/PlatformLabel.js
+++ src/components/elements/misc/IconLabels/PlatformLabel.js
@@ -1,4 +1,4 @@
-import {BsApple, BsWindows} from "react-icons/bs";
+import {BsApple, BsWindows, BsPhone} from "react-icons/bs";
 import {VscTerminalLinux} from "react-icons/vsc";
 import React from "react";
 
@@ -6,7 +6,8 @@ export default function PlatformIcon({platform}) {
     const platforms = {
         "Win32": <BsWindows size={20} style={{marginRight: 5}}/>,
         "MacIntel": <BsApple size={20} style={{marginRight: 5}}/>,
-        "Linux x86_64": <VscTerminalLinux size={20} style={{marginRight: 5}}/>
+        "Linux x86_64": <VscTerminalLinux size={20} style={{marginRight: 5}}/>,
+        "iPhone": <BsPhone size={20} style={{marginRight: 5}}/>
     }
     if (platform in platforms) {
         return <>{platforms[platform]} {platform}</>
