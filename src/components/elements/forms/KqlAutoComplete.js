01bd2d9808b50f83cbb9b1198c4443c5ea60acfd
risto <risto.kowaczewski@gmail.com>
2023-01-15 23:13:36 +0100

    autocomplete

--- src/components/elements/forms/KqlAutoComplete.js
+++ src/components/elements/forms/KqlAutoComplete.js
@@ -1,8 +1,8 @@
-import React, {useEffect, useRef, useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import TextField from '@mui/material/TextField';
 import Autocomplete from '@mui/material/Autocomplete';
 import CircularProgress from '@mui/material/CircularProgress';
-import {asyncRemote, getError} from "../../../remote_api/entrypoint";
+import {asyncRemote} from "../../../remote_api/entrypoint";
 
 export default function KqlAutoComplete() {
 
@@ -10,6 +10,7 @@ export default function KqlAutoComplete() {
     const [options, setOptions] = React.useState([]);
     const [progress, setProgress] = React.useState(false);
     const [open, setOpen] = React.useState(false);
+    const [current, setCurrent] = React.useState(false);
 
     useEffect(() => {
         let isSubscribed = true;
@@ -20,11 +21,9 @@ export default function KqlAutoComplete() {
             method: "get",
         }).then((response) => {
             if (response && isSubscribed) {
-
-                let options = response.data
-
+                let options = response.data.next
+                setCurrent(response.data.current)
                 if (typeof options !== "undefined" && options !== null) {
-                    console.log(options)
                     setOptions(options);
                 } else {
                     setOptions([])
@@ -44,13 +43,36 @@ export default function KqlAutoComplete() {
     }, [kql])
 
     const handleTyping = (ev, v, reason) => {
-        console.log(v, reason)
         setKql({...kql, value: v})
     }
 
     const handleSelect = (event, v) => {
-        if(v) {
-            setKql({...kql, value: kql.value + " " + v.value})
+        if (v?.value) {
+            let base
+            if (current?.value !== "" && current?.token === v.token) {
+                base = kql.value.substring(0, kql.value.length - current.value.length)
+            } else {
+                base = kql.value
+            }
+
+
+            if (v.space === null) {
+                // APPEND_NONE
+                v = base + v.value
+            } else if (v.space === 0) {
+                // APPEND_NONE
+                v = base + " " + v.value + " "
+            } else if (v.space === -1) {
+                // APPEND_BEFORE
+                v = base + " " + v.value
+            } else if (v.space === 1) {
+                // APPEND_AFTER
+                v = base + v.value + " "
+            } else {
+                v = base + v.value
+            }
+
+            setKql({...kql, value: v})
         }
     }
 
@@ -77,7 +99,15 @@ export default function KqlAutoComplete() {
                 return <div {...props} style={{display: "flex"}}>
                     <span style={{display: "flex", width: 100, justifyContent: "left"}}>
                         <span
-                            style={{backgroundColor: "#ef6c00", color: "white", padding: "1px 5px", borderRadius: 5, marginRight: 5, fontSize: 11, textTransform: "uppercase"}}>{option.token}</span>
+                            style={{
+                                backgroundColor: option.color,
+                                color: "white",
+                                padding: "1px 5px",
+                                borderRadius: 5,
+                                marginRight: 5,
+                                fontSize: 11,
+                                textTransform: "uppercase"
+                            }}>{option.token}</span>
                     </span>
                     <span style={{display: "table-cell", width: 700}}>{option.value}</span>
                     <span style={{display: "table-cell", fontSize: 11, color: "gray"}}>{option.desc}</span>
@@ -95,7 +125,7 @@ export default function KqlAutoComplete() {
                                                             {progress ?
                                                                 <CircularProgress color="inherit" size={20}
                                                                                   style={{marginRight: 30}}
-                                                                /> : <div style={{marginRight: 30}}></div>}
+                                                                /> : <div style={{marginRight: 30}}/>}
                                                             {params.InputProps.endAdornment}
                                                         </>
                                                     ),
