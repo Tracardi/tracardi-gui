3244fbcc34b1c13a9dfe613458b95a502f330f74
risto <risto.kowaczewski@gmail.com>
2023-04-25 23:32:56 +0200

    Version 0.8.2-dev

--- build-http.sh
+++ build-http.sh
@@ -1,3 +1,3 @@
 git rev-parse HEAD > public/revision.txt
-docker build . --rm --no-cache -f http.Dockerfile -t tracardi/tracardi-gui:0.8.1-dev
-docker push tracardi/tracardi-gui:0.8.1-dev
+docker build . --rm --no-cache -f http.Dockerfile -t tracardi/tracardi-gui:0.8.2-dev
+docker push tracardi/tracardi-gui:0.8.2-dev
