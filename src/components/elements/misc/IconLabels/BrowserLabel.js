b1d19e3969a14a83688519bf249e69106febad7a
risto <risto.kowaczewski@gmail.com>
2023-07-13 09:27:14 +0200

    Minor changes.

--- src/components/elements/misc/IconLabels/BrowserLabel.js
+++ src/components/elements/misc/IconLabels/BrowserLabel.js
@@ -1,6 +1,7 @@
 import {FaSafari, FaChrome, FaFirefox, FaOpera, FaEdge} from "react-icons/fa";
 import React from "react";
 import {FaRobot} from "react-icons/fa";
+import {capitalizeString} from "../EventTypeTag";
 
 export default function BrowserLabel({browser, version = null, robot = false}) {
     const browsers = {
@@ -12,7 +13,7 @@ export default function BrowserLabel({browser, version = null, robot = false}) {
     }
     const _browser = browser.toLowerCase()
     if (_browser in browsers) {
-        return <>{browsers[_browser]} {browser} <span style={{marginLeft: 5}}>v.{version}</span> {robot &&
+        return <>{browsers[_browser]} {capitalizeString(browser)} <span style={{marginLeft: 5}}>{version && `v.${version}`}</span> {robot &&
         <FaRobot size={20} style={{marginLeft: 10}}/>} </>
     }
 
