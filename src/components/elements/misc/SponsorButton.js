0bf20207259fe5cdcdfbb10e42a5f5c174d08706
risto <risto.kowaczewski@gmail.com>
2023-08-21 11:00:41 +0200

    Sponsor button event

--- src/components/elements/misc/SponsorButton.js
+++ src/components/elements/misc/SponsorButton.js
@@ -2,15 +2,27 @@ import Button from "../forms/Button";
 import React from "react";
 import "./NeedHelpButton.css";
 import {BsHeart} from "react-icons/bs";
+import {track} from "../../../remote_api/track";
+import version from "../../../misc/version";
 
 export default function SponsorButton() {
 
     const external = (url, newWindow = false) => {
         if (newWindow === true) {
-            return () => window.open(url, '_blank', 'location=yes,scrollbars=yes,status=yes')
+            window.open(url, '_blank', 'location=yes,scrollbars=yes,status=yes')
         } else {
-            return () => window.location.href = url;
+            window.location.href = url;
         }
+    }
+
+    const handleClick = () => {
+        track("9d9230c3-def2-451a-9b52-c554686f3e27", 'tracardi-sponsorship-clicked', {
+            version: version()
+        }).then(() => {
+            external("http://www.opencollective.com/tracardi-cdp")
+        }).catch(() => {
+            external("http://www.opencollective.com/tracardi-cdp")
+        })
     }
 
     return <div>
@@ -18,7 +30,7 @@ export default function SponsorButton() {
                 selected={true}
                 icon={<BsHeart size={20}/>}
                 style={{padding: "6px 14px", marginBottom: 10, marginRight: 5}}
-                onClick={external("http://www.opencollective.com/tracardi-cdp")}
+                onClick={handleClick}
         ></Button>
     </div>
 
