bdff54d7cdc292719aae8a5749b66eb3932a4a58
risto <risto.kowaczewski@gmail.com>
2023-12-21 02:01:54 +0100

    Dark mode

--- src/themes.js
+++ src/themes.js
@@ -1,46 +1,46 @@
 import { createTheme} from '@mui/material/styles';
 
-// export const stagingTheme = createTheme({
-//     typography: {
-//         "fontFamily": `"IBM Plex Sans", "Arial", sans-serif`,
-//         "fontSize": 15,
-//         "fontWeightLight": 300,
-//         "fontWeightRegular": 400,
-//         "fontWeightMedium": 500
-//     },
-//     palette: {
-//         primary: {
-//             main: '#3B82F6',  // #1976d2
-//             light: '#e1f5fe'  // e1f5fe
-//         },
-//         secondary: {
-//             main: '#EF6C00',
-//         },
-//         error: {
-//             main: "#d81b60",
-//         },
-//         success: {
-//             main: "#43a047",
-//         },
-//         gray: {
-//             main: "#ccc"
-//         },
-//         background: {
-//             default: 'whitesmoke',  // whitesmoke
-//             paper: "white" // white
-//         },
-//         text: {
-//             primary: '#000',
-//             secondary: '#444',
-//         },
-//         common: {
-//             white: "white",
-//             black: "black"
-//         }
-//     },
-// });
-
 export const stagingTheme = createTheme({
+    typography: {
+        "fontFamily": `"IBM Plex Sans", "Arial", sans-serif`,
+        "fontSize": 15,
+        "fontWeightLight": 300,
+        "fontWeightRegular": 400,
+        "fontWeightMedium": 500
+    },
+    palette: {
+        primary: {
+            main: '#3B82F6',  // #1976d2
+            light: '#e1f5fe'  // e1f5fe
+        },
+        secondary: {
+            main: '#EF6C00',
+        },
+        error: {
+            main: "#d81b60",
+        },
+        success: {
+            main: "#43a047",
+        },
+        gray: {
+            main: "#ccc"
+        },
+        background: {
+            default: 'whitesmoke',  // whitesmoke
+            paper: "white" // white
+        },
+        text: {
+            primary: '#000',
+            secondary: '#444',
+        },
+        common: {
+            white: "white",
+            black: "black"
+        }
+    },
+});
+
+export const darkTheme = createTheme({
     typography: {
         "fontFamily": `"IBM Plex Sans", "Arial", sans-serif`,
         "fontSize": 15,
