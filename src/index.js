79a7921da078bb0fbf4940abd4caf4ab88e96288
risto <risto.kowaczewski@gmail.com>
2024-03-07 18:38:03 +0100

    removed sentry

--- src/index.js
+++ src/index.js
@@ -10,27 +10,6 @@ import {stagingTheme} from "./themes";
 import {ThemeProvider, StyledEngineProvider} from "@mui/material/styles";
 import {ConfirmProvider} from "material-ui-confirm";
 import ApiUrlSelector from "./components/ApiUrlSelector";
-import * as Sentry from "@sentry/react";
-import {Integrations} from "@sentry/tracing";
-import envs from "./envs";
-
-if (envs.license === 'open-source' && (!process.env.NODE_ENV || process.env.NODE_ENV !== 'development')) {
-    Sentry.init({
-        dsn: "https://2721a09bf1144c10930117609a67f4d5@o1093519.ingest.sentry.io/6112822",
-        integrations: [new Integrations.BrowserTracing(), new Sentry.Replay()],
-
-        // Set tracesSampleRate to 1.0 to capture 100%
-        // of transactions for performance monitoring.
-        // We recommend adjusting this value in production
-        tracesSampleRate: 1.0,
-        // This sets the sample rate to be 10%. You may want this to be 100% while
-        // in development and sample at a lower rate in production
-        replaysSessionSampleRate: 0.1,
-        // If the entire session is not sampled, use the below sample rate to sample
-        // sessions when an error occurs.
-        replaysOnErrorSampleRate: 1.0
-    });
-}
 
 const container = document.getElementById('root');
 const root = createRoot(container);
