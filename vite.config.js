51a1ea4b54599ba4b4eb8228cbe1dc7e6eab8495
risto <risto.kowaczewski@gmail.com>
2024-03-21 06:29:10 +0100

    Libs Upgrade

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: '/',
     plugins: [react(), svgr({
         svgrOptions: {
             icon: true,
